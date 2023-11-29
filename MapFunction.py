012ae28671613f3a384fa11beb6782fabc7c1c06
dileepkumar4488 <152370633+dileepkumar4488@users.noreply.github.com>
2023-11-29 12:34:59 +0530

    Create MapFunction.py

---
+++ MapFunction.py
@@ -0,0 +1,5 @@
+list1=[1,2,3,4,5,6,7]
+result= list(map(lambda num : num * 3, list1))
+print(f"Given input list is: {list1}")
+print("--------------------------------------------------------------------------------")
+print(f"The output result with three times of input numbers is: {result}")
