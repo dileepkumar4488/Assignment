9947e63110cfd7cb58f3cec5faaf03d87cf2a3af
dileepkumar4488 <152370633+dileepkumar4488@users.noreply.github.com>
2024-01-06 12:53:04 +0530

    Create myDictionary.py

---
+++ myDictionary.py
@@ -0,0 +1,8 @@
+ascii_alphabets_map = {}
+
+my_alphabets = ("abcdefghijklmnopqrstuvwxyz")
+
+for each_char in my_alphabets:
+    ascii_alphabets_map[each_char] = ord(each_char)
+
+print(ascii_alphabets_map)
